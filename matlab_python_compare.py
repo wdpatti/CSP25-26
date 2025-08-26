77cfea1dd9d4c172996dec71312fee9d74067f0d
Will Patti <wdppostcard@gmail.com>
2025-08-26 12:40:40 -0400

    first day with python

---
+++ matlab_python_compare.py
@@ -0,0 +1,8 @@
+a = 2 # assign the variable a the value 2
+b = 3 # assign the variable b the value 3
+c = 11 # assign the variable 3 the value 11
+
+
+x = (c - b) / a
+
+print(f"The solution for x is: {x}")
