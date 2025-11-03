a2e8a3b423c910d713a2de3c92cf9e7b48c8c29d
Will Patti <28pattiw@charlottelatin.org>
2025-11-03 12:40:24 -0500

    initial code from document

---
+++ projects/ladybug/main.py
@@ -0,0 +1,62 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("lightgreen")
+turtle.setup(width=600, height=600)
+
+t = turtle.Turtle()
+t.pensize(2)
+
+t.penup()
+t.goto(-100, 50)
+t.pendown()
+t.fillcolor("red")
+t.begin_fill()
+t.circle(100) # Draw a larger body as a circle
+t.end_fill()
+
+t.penup()
+t.goto(-100, 150) # PosiIon for the head
+t.pendown()
+t.fillcolor("black")
+t.begin_fill()
+t.circle(40) # Draw the head
+t.end_fill()
+
+t.penup()
+t.goto(-100, 150)
+t.setheading(-90)
+t.pendown()
+t.pensize(3)
+t.forward(200) # Line separaIng the body
+
+spots_posiIons = [(-50, 50), (50, 50), (-75, 0), (75, 0), (-50, -50), (50, -50)]
+for spot_x, spot_y in spots_posiIons:
+    t.penup()
+    t.goto(-100 + spot_x, 50 + spot_y)
+    t.pendown()
+    t.fillcolor("black")
+    t.begin_fill()
+    t.circle(15) # Draw the spots
+    t.end_fill()
+
+legs_posiIons = [(-100, 50), (-100, 0), (-100, -50), (100, 50), (100, 0),
+(100, -50)]
+for leg_x, leg_y in legs_posiIons:
+    t.penup()
+    t.goto(-100 + leg_x, 50 + leg_y)
+    t.pendown()
+    t.setheading(leg_x) # Adjust leg direcIon
+    t.forward(50) # Draw the legs
+
+t.penup()
+t.goto(-115, 180) # LeY antenna posiIon
+t.setheading(60)
+t.pendown()
+t.setheading(120)
+t.pendown()
+t.forward(60)
+
+t.hideturtle()
+
+screen.exitonclick()
