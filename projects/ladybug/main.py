bfc94f553ed0845f24dfef08ba989d074d304ff1
Will Patti <28pattiw@charlottelatin.org>
2025-11-03 12:57:36 -0500

    fix ladybug

--- projects/ladybug/main.py
+++ projects/ladybug/main.py
@@ -16,7 +16,7 @@ t.circle(100) # Draw a larger body as a circle
 t.end_fill()
 
 t.penup()
-t.goto(-100, 150) # Position for the head
+t.goto(-100, 160) # Position for the head
 t.pendown()
 t.fillcolor("black")
 t.begin_fill()
@@ -24,16 +24,16 @@ t.circle(40) # Draw the head
 t.end_fill()
 
 t.penup()
-t.goto(-100, 150)
+t.goto(-100, 170)
 t.setheading(-90)
 t.pendown()
 t.pensize(3)
-t.forward(200) # Line separating the body
+t.forward(120) # Line separating the body
 
 spots_positions = [(-50, 50), (50, 50), (-75, 0), (75, 0), (-50, -50), (50, -50)]
 for spot_x, spot_y in spots_positions:
     t.penup()
-    t.goto(-100 + spot_x, 50 + spot_y)
+    t.goto(-115 + spot_x, 150 + spot_y)
     t.pendown()
     t.fillcolor("black")
     t.begin_fill()
@@ -43,15 +43,18 @@ for spot_x, spot_y in spots_positions:
 legs_positions = [(-100, 50), (-100, 0), (-100, -50), (100, 50), (100, 0), (100, -50)]
 for leg_x, leg_y in legs_positions:
     t.penup()
-    t.goto(-100 + leg_x, 50 + leg_y)
+    t.goto(-100 + leg_x, 150 + leg_y)
     t.pendown()
-    t.setheading(leg_x) # Adjust leg direction
+    t.setheading(leg_x/100*20-90) # Adjust leg direction
     t.forward(50) # Draw the legs
 
 t.penup()
-t.goto(-115, 180) # LeY antenna position
+t.goto(-115, 220) # LeY antenna position
 t.setheading(60)
 t.pendown()
+t.forward(60)
+t.penup()
+t.goto(-90, 220) # LeY antenna position
 t.setheading(120)
 t.pendown()
 t.forward(60)
