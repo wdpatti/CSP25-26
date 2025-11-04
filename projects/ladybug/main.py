01522803bb61044f840a53cffe1c07a4d73842ae
Will Patti <28pattiw@charlottelatin.org>
2025-11-04 13:20:04 -0500

    four corners turtles

--- projects/ladybug/main.py
+++ projects/ladybug/main.py
@@ -2,16 +2,15 @@ import turtle
 
 screen = turtle.Screen()
 screen.bgcolor("lightgreen")
-turtle.setup(width=600, height=800)
+turtle.setup(width=600, height=600)
 
 t = turtle.Turtle()
 t.pensize(2)
 
-x_off = -50
-for i in range(2):
+for x_off, y_off in [(-50, -50), (250, -50), (-50, -325), (250, -325)]:
     t.setheading(0)
     t.penup()
-    t.goto(x_off + -100, 50)
+    t.goto(x_off + -100, y_off + 50)
     t.pendown()
     t.fillcolor("red")
     t.begin_fill()
@@ -19,7 +18,7 @@ for i in range(2):
     t.end_fill()
 
     t.penup()
-    t.goto(x_off + -100, 225) # Position for the head
+    t.goto(x_off + -100, y_off + 225) # Position for the head
     t.pendown()
     t.fillcolor("black")
     t.begin_fill()
@@ -27,7 +26,7 @@ for i in range(2):
     t.end_fill()
 
     t.penup()
-    t.goto(x_off + -100, 225)
+    t.goto(x_off + -100, y_off + 225)
     t.setheading(-90)
     t.pendown()
     t.pensize(3)
@@ -36,7 +35,7 @@ for i in range(2):
     spots_positions = [(-50, 50), (50, 50), (-75, 0), (75, 0), (-50, -50), (50, -50)]
     for spot_x, spot_y in spots_positions:
         t.penup()
-        t.goto(x_off + -115 + spot_x, 150 + spot_y)
+        t.goto(x_off + -115 + spot_x, y_off + 150 + spot_y)
         t.pendown()
         t.fillcolor("black")
         t.begin_fill()
@@ -46,22 +45,21 @@ for i in range(2):
     legs_positions = [(-85, 50), (-100, 0), (-85, -50), (85, 50), (100, 0), (85, -50)]
     for leg_x, leg_y in legs_positions:
         t.penup()
-        t.goto(x_off + -100 + leg_x, 150 + leg_y)
+        t.goto(x_off + -100 + leg_x, y_off + 150 + leg_y)
         t.pendown()
         t.setheading(leg_x/100*30-90) # Adjust leg direction
         t.forward(50) # Draw the legs
 
     t.penup()
-    t.goto(x_off + -115, 275) # LeY antenna position
+    t.goto(x_off + -115, y_off + 275) # LeY antenna position
     t.setheading(60)
     t.pendown()
     t.forward(60)
     t.penup()
-    t.goto(x_off + -90, 275) # LeY antenna position
+    t.goto(x_off + -90, y_off + 275) # LeY antenna position
     t.setheading(120)
     t.pendown()
     t.forward(60)
-    x_off = 250
 
 
 
